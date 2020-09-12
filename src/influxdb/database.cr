647748754d3e44bf534fede657949bf6332b24dc
Hữu Hà ZSmart <business@zsmart.tech>
2020-09-12 14:53:53 +0700

    Update database.cr

--- src/influxdb/database.cr
+++ src/influxdb/database.cr
@@ -1,67 +1,31 @@
 module InfluxDB
-  struct Database
-    getter :name
+  struct Databases
+    include Enumerable(Database)
     delegate :query, to: @client
 
-    # @@processed = Channel(HTTP::Client::Response)
-    # spawn do
-    #   loop do
-    #     resp = @@processed.receive
-    #   end
-    # end
-
-    def initialize(@client : Client, @name : String)
-      @mutex = Mutex.new
+    def initialize(@client : Client)
     end
 
-    # https://docs.influxdata.com/influxdb/latest/query_language/database_management/#delete-a-database-with-drop-database
-    def drop
-      query "DROP DATABASE #{name}"
-      true
+    def create(name)
+      query "CREATE DATABASE #{name}"
+      self[name]
     end
 
-    def select(fields = "*")
-      Query.new(@client, name).select(fields)
+    def [](name : String)
+      Database.new(@client, name)
     end
 
-    def write(point_value : PointValue)
-      write [point_value]
-    end
-
-    def write(point_values : Array(PointValue))
-      body = String.build { |str|
-        point_values.each_with_index do |pv, i|
-          pv.to_s(str)
-          str << "\n" unless i == point_values.size - 1
-        end
-      }.strip
-
-      send_write(body).status_code == 204
-    end
-
-    private def send_write(body)
-      @mutex.synchronize do
-        @client.post "/write?db=#{name}&precision=ms",
-          HTTP::Headers{
-            "Content-Type" => "application/octet-stream",
-          },
-          body
+    def all
+      res = query("SHOW DATABASES")
+      dbs = [] of Database
+      res[0]["series"][0]["values"].as_a.each do |arr|
+        dbs << Database.new(@client, arr[0].as_s)
       end
+      dbs
     end
 
-    def write(series : String, fields : Fields, tags = Tags.new, timestamp : Time? = nil)
-      timestamp = Time.utc if timestamp.nil?
-      write PointValue.new(series, tags: tags, fields: fields, timestamp: timestamp)
-    end
-
-    def write(series : String, value : Value, tags = Tags.new, timestamp : Time? = nil)
-      write series, Fields{:value => value}, tags: tags, timestamp: timestamp
-    end
-
-    def write
-      pw = PointsWriter.new
-      yield pw
-      write pw.points
+    def each
+      all.each { |db| yield db }
     end
   end
 end
