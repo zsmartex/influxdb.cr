f242bcb624a94b30a96783065767a7f2eb53a066
Hữu Hà ZSmart <business@zsmart.tech>
2020-09-12 14:54:00 +0700

    Update database.cr

--- src/influxdb/database.cr
+++ src/influxdb/database.cr
@@ -1,31 +1,63 @@
 module InfluxDB
-  struct Databases
-    include Enumerable(Database)
-    delegate :query, to: @client
+  struct Database
+    getter :name
 
-    def initialize(@client : Client)
+    # @@processed = Channel(HTTP::Client::Response)
+    # spawn do
+    #   loop do
+    #     resp = @@processed.receive
+    #   end
+    # end
+
+    def initialize(@client : Client, @name : String)
+      @mutex = Mutex.new
     end
 
-    def create(name)
-      query "CREATE DATABASE #{name}"
-      self[name]
+    def query(q)
+      @client.query(q, db: name)
     end
 
-    def [](name : String)
-      Database.new(@client, name)
+    # https://docs.influxdata.com/influxdb/latest/query_language/database_management/#delete-a-database-with-drop-database
+    def drop
+      query "DROP DATABASE #{name}"
+      true
     end
 
-    def all
-      res = query("SHOW DATABASES")
-      dbs = [] of Database
-      res[0]["series"][0]["values"].as_a.each do |arr|
-        dbs << Database.new(@client, arr[0].as_s)
+    def select(fields = "*")
+      Query.new(@client, name).select(fields)
+    end
+
+    def write_point(point_value : PointValue, flush : String?)
+      body = String.build { |str|
+        str << point_value.to_s(str)
+      }.strip
+
+      send_write(body, flush)
+    end
+
+    def write_point(series : String, value : Value, tags = Tags.new, timestamp : Time? = nil)
+      write_point series, Fields{:value => value}, tags: tags, timestamp: timestamp
+    end
+
+    def write_point(series : String, fields : Fields, tags = Tags.new, timestamp : Time? = nil)
+      timestamp = Time.utc if timestamp.nil?
+      write_point PointValue.new(series, tags: tags, fields: fields, timestamp: timestamp)
+    end
+
+    private def send_write(body, flush : String? = "ms")
+      @mutex.synchronize do
+        @client.post "/write?db=#{name}&precision=#{flush}",
+          HTTP::Headers{
+            "Content-Type" => "application/octet-stream",
+          },
+          body
       end
-      dbs
     end
 
-    def each
-      all.each { |db| yield db }
+    def write
+      pw = PointsWriter.new
+      yield pw
+      write pw.points
     end
   end
 end
